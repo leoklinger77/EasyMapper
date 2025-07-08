7aea9bfc8d57f5953fef12230e8d6877fcdf8a54
klinger <klinger@klingersystem.com.br>
2025-07-07 22:45:26 -0300

    improvement in the 'AddMapping' method

--- src/EasyMapper/Mapper/MapperProfile.cs
+++ src/EasyMapper/Mapper/MapperProfile.cs
@@ -16,8 +16,15 @@
 		internal void AddMapping<TSource, TDestination, TValue>(
 			Expression<Func<TDestination, TValue>> destMember,
 			Func<TSource, TValue> mapFunc) {
-			var memberName = ((MemberExpression)destMember.Body).Member.Name;
-			_customMappings[(typeof(TSource), typeof(TDestination), memberName)] = mapFunc;
+			MemberExpression memberExpr = destMember.Body as MemberExpression;
+
+			if (memberExpr == null && destMember.Body is UnaryExpression unaryExpr && unaryExpr.Operand is MemberExpression operandExpr)
+				memberExpr = operandExpr;
+
+			if (memberExpr == null)
+				throw new InvalidOperationException("The provided expression is not a valid MemberExpression.");
+			
+			_customMappings[(typeof(TSource), typeof(TDestination), memberExpr.Member.Name)] = mapFunc;
 		}
 
 		public bool TryGetCustomMapping<TSource, TDestination>(string destProp, out Func<TSource, object?>? func) {
